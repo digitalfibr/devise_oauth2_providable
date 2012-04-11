f2ce52d369444dfeabfadd0570942d5eec6e528f
HumanProxy <benoit.molenda@gmail.com>
2012-04-11 17:18:05 +0200

    fix missing namespace specification

--- app/controllers/devise/oauth2_providable/tokens_controller.rb
+++ app/controllers/devise/oauth2_providable/tokens_controller.rb
@@ -6,7 +6,7 @@ class Devise::Oauth2Providable::TokensController < ApplicationController
     @refresh_token = oauth2_current_refresh_token
     @refresh_token ||= oauth2_current_client.send(Devise::Oauth2Providable::ABSTRACT(:refresh_token_plur)).create!(:user => current_user)
     @access_token = @refresh_token.send(Devise::Oauth2Providable.ABSTRACT(:access_token_plur)).create!(
-      ABSTRACT(:client_sym) => oauth2_current_client,
+      Devise::Oauth2Providable.ABSTRACT(:client_sym) => oauth2_current_client,
       :user => current_user
     )
     render :json => @access_token.token_response
