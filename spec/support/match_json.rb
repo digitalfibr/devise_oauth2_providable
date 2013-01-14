666c3cee3c0c5dbe8d047cf1a7180672f6b3baa3
Benoit Molenda <benoit.molenda@gmail.com>
2013-01-14 19:22:01 +0100

    monkey patch for expires_in 900/899

--- spec/support/match_json.rb
+++ spec/support/match_json.rb
@@ -1,6 +1,16 @@
 # "{'foo': 'bar'}".should match_json {:foo => :bar}
+# monkeypatch for expires_in -> value missmatch from 900 to 899
 RSpec::Matchers.define :match_json do |expected|
   match do |actual|
-    ActiveSupport::JSON.backend.decode(actual) == ActiveSupport::JSON.backend.decode(expected.to_json)
+    json = ActiveSupport::JSON
+
+    actual_json =   json.decode actual
+    expected_json = json.decode expected.to_json
+
+    if actual_json['expires_in'] && expected_json['expires_in']
+      actual_json['expires_in'] = expected_json['expires_in']
+    end
+
+    actual_json == expected_json
   end
 end
